5344c5c00f27b3891e128358988d05c90190b581
lingyin1234 <lilu3964@colorado.edu>
2019-02-01 15:35:45 -0700

    Lab 2 finished

---
+++ lab2_scripts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Authors : Lingyin Lu
+# Date: 1/30/2019
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+echo "Enter a regex: "
+read regex
+echo "Enter a filename: "
+read filename
+grep -E "$regex" $filename
+
+grep -E "[0-9]{3}-[0-9]{3}-[0-9]{4}$" $1 | wc -l
+grep -E "[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-z]{2,}" $1 | wc -l
+
+grep -E "[0-9]{3}-[0-9]{3}-[0-9]{4}$" $1 | grep -E "303" >phone_results.txt
+
+grep -E "[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-z]{2,}" $1 | grep -E "geocities.com" > email_results.txt
+
+
+grep -E $2 $1 > command_results.txt
+
+
