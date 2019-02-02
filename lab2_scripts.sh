a982fadc7b8c87b800aaa8d0dcd14ee6d6513ae0
lingyin1234 <46982312+lingyin1234@users.noreply.github.com>
2019-02-01 17:24:51 -0700

    Update lab2_scripts.sh

--- lab2_scripts.sh
+++ lab2_scripts.sh
@@ -10,10 +10,10 @@ echo "Enter a filename: "
 read filename
 grep -E "$regex" $filename
 
-grep -E "[0-9]{3}-[0-9]{3}-[0-9]{4}$" $1 | wc -l
+grep -E "[0-9]{3}-[0-9]{3}-[0-9]{4}$" $1 
 grep -E "[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-z]{2,}" $1 | wc -l
 
-grep -E "[0-9]{3}-[0-9]{3}-[0-9]{4}$" $1 | grep -E "303" >phone_results.txt
+grep -E "303" $1 >phone_results.txt
 
 grep -E "[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-z]{2,}" $1 | grep -E "geocities.com" > email_results.txt
 
